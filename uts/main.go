3d48a181ed8e5a78d5f25bbe7e40a9d02b1fb344
wusphinx <wu.sphinx@gmail.com>
2022-10-18 11:03:16 +0800

    feat: add ipc and pid ns

--- uts/main.go
+++ uts/main.go
@@ -10,7 +10,14 @@ import (
 func main() {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS |
+			// 实际上是一个位操作是: 67108864|134217728
+			// refer: https://man7.org/linux/man-pages/man2/clone.2.html
+			syscall.CLONE_NEWIPC |
+			// 运行后看到当前pid为1
+			// # echo $$
+			// 1
+			syscall.CLONE_NEWPID,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
