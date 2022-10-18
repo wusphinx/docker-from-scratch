15d58fb989d9fb112a4987d3a8798da20d06314e
wusphinx <wu.sphinx@gmail.com>
2022-10-18 12:25:59 +0800

    feat: add user ns

--- uts/main.go
+++ uts/main.go
@@ -17,8 +17,48 @@ func main() {
 			// 运行后看到当前pid为1
 			// # echo $$
 			// 1
-			syscall.CLONE_NEWPID,
+			syscall.CLONE_NEWPID |
+			// mount namespace
+			// # mount -t proc proc /pro
+			syscall.CLONE_NEWNS|
+			syscall.CLONE_NEWUSER,
+			// refer: https://github.com/xianlubird/mydocker/issues/3
+			/*
+			以下两种情况，会导致UidMappings/GidMappings中设置了非当前进程所属UID和GID的相关数值：
+			1. HostID非本进程所有（与Getuid()和Getgid()不等）
+			2. Size大于1 （则肯定包含非当前进程的UID和GID）
+			则需要Host机使用Root权限才能正常执行此段代码。
+			*/
+			// 以root扫行后，当前id是10086，而宿主机的id并不是10086，以此可以说明user namespace生效了
+			UidMappings: []syscall.SysProcIDMap{
+				{
+					ContainerID: 10086,
+					HostID:      syscall.Getuid(),
+					Size:        1,
+				},
+				{
+					ContainerID: 10010,
+					HostID:      syscall.Getgid() + 1,
+					Size:        1,
+				},
+			},
+			GidMappings: []syscall.SysProcIDMap{
+				{
+					ContainerID: 10086,
+					HostID:      syscall.Getgid(),
+					Size:        1,
+				},
+				{
+					ContainerID: 10010,
+					HostID:      syscall.Getgid() + 1,
+					Size:        1,
+				},
+			},
+			
 	}
+	// 以下代码会报：fork/exec /usr/bin/sh: operation not permitted exit status 1
+	// cmd.SysProcAttr.Credential = &syscall.Credential{Uid:uint32(1), Gid:uint32(1)}
+	
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
